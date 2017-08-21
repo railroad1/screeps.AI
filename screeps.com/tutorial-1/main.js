5138cce91e98aea91bdbcb3b57b5b7149ffd1406
Railroad1 <joshwankowski@gmail.com>
2017-08-21 12:53:01 -0500

    nothing

--- screeps.com/tutorial-1/main.js
+++ screeps.com/tutorial-1/main.js
@@ -1,3 +1,8 @@
-module.exports.loop = function () {
 
+module.exports.loop = function () {
+    var creep = Game.creeps['Harvester1'];
+    var sources = creep.room.find(FIND_SOURCES);
+    if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+        creep.moveTo(sources[0]);
+    }
 }
